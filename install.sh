83eebd04c9be6d795983fc34f43e11aa08cc9c64
AlexandreAdam <adam.alexandre01123@gmail.com>
2020-11-16 20:27:21 -0500

    reset install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!usr/bin/bash
-#conda create -n fbl python=3.6 -y
-#conda activate fbl
-#conda install -c conda-forge schwimmbad
+conda create -n fbl python=3.6 -y
+conda activate fbl
+conda install -c conda-forge schwimmbad
 #sudo apt-get install gfortran
 #wget https://wwwmpa.mpa-garching.mpg.de/gadget/gadget-2.0.7.tar.gz
-#pip install -r requirements.txt
+pip install -r requirements.txt
 cd ..
-#git clone https://github.com/rtqichen/ffjord.git
+git clone https://github.com/rtqichen/ffjord.git
 git clone https://github.com/AlexandreAdam/LensTools.git 
 cd LensTools
 python setup.py develop
