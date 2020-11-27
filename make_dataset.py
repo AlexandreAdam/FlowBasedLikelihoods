2b09ab3cca6f918ddd02254844257643d616df30
AlexandreAdam <adam.alexandre01123@gmail.com>
2020-11-26 20:20:18 -0500

    Data exploration and density estimation with mcmc

--- make_dataset.py
+++ make_dataset.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 from astropy.io import fits
-from lenstools import ConvergenceMap
+try:
+    from lenstools import ConvergenceMap
+    _lenstools=True
+except ImportError:
+    _lenstools=False
 import os, glob
 
 """
@@ -15,7 +19,7 @@ cosmology = ["H", "OMEGA_M", "OMEGA_L", "W0", "WA", "Z", "ANGLE"]
 ell_bins = [f"ell{i}" for i in range(38 - 1 )]
 
 def main(datapath, output_file):
-    ell = np.logspace(2, np.log10(5000), 38)  # multipole bin edges
+    ell = np.logspace(np.log10(500), np.log10(5000), 38)  # multipole bin edges
     data = pd.DataFrame(columns=cosmology+ell_bins)
     data.to_csv(output_file) # save columns to file
     # make a single row to receive data
@@ -26,13 +30,16 @@ def main(datapath, output_file):
         for d in dirs:
             for root, _, files in os.walk(os.path.join(datapath, d)):  
                 for conv_map_fit in files:
-                    conv_map = ConvergenceMap.load(os.path.join(root,conv_map_fit))
-                    hdul = fits.open(os.path.join(root, conv_map_fit))[0].header
-                    for c_param in cosmology:
-                        data[c_param] = hdul[c_param]
-                    l, power_spectrum = conv_map.powerSpectrum(ell)
-                    data[ell_bins] = power_spectrum
-                    data.to_csv(output_file, mode="a", header=False)
+                    try:
+                        conv_map = ConvergenceMap.load(os.path.join(root,conv_map_fit))
+                        hdul = fits.open(os.path.join(root, conv_map_fit))[0].header
+                        for c_param in cosmology:
+                            data[c_param] = hdul[c_param]
+                        l, power_spectrum = conv_map.powerSpectrum(ell)
+                        data[ell_bins] = power_spectrum
+                        data.to_csv(output_file, mode="a", header=False)
+                    except:
+                        continue
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
