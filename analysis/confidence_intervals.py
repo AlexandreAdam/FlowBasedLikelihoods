f6fff716a1687a0c30062f7b8dec7d1c4d65ee14
Olivier <ovincent@astro.umontreal.ca>
2020-12-08 20:23:40 -0500

    nongauss

--- analysis/confidence_intervals.py
+++ analysis/confidence_intervals.py
@@ -18,17 +18,19 @@ def main(datapath):
     c2_l = ell * (ell + 1) * (mean - 2 * std)/2/np.pi
 
     plt.figure()
-    plt.title("Power spectra", fontsize=13)
-    plt.plot(ell, ell * (ell + 1) * mean/2/np.pi, "c-")
-    plt.plot(ell, c1, "c-")
-    plt.plot(ell, c1_l, "c-")
-    plt.plot(ell, c2, "c-")
-    plt.plot(ell, c2_l, "c-")
-    plt.xlabel(r"$\ell$", fontsize=13)
-    plt.ylabel(r"$\ell  (\ell + 1)  P_\ell / 2 \pi$", fontsize=13)
+#    plt.title("Power spectra", fontsize=13)
+    plt.plot(ell, ell * (ell + 1) * mean/2/np.pi, "k-")
+    plt.plot(ell, c1, "k-",lw=.5,alpha=0.8)
+    plt.plot(ell, c1_l, "k-",lw=.5,alpha=0.8)
+    plt.plot(ell, c2, "k-",lw=.5,alpha=0.6)
+    plt.plot(ell, c2_l, "k-",lw=.5,alpha=0.6)
+    plt.xlabel(r"$\ell$", fontsize=14)
+    plt.ylabel(r"$\ell  (\ell + 1)  P_\ell / 2 \pi$", fontsize=14)
+    
+    plt.xlim(ell.min(),ell.max())
 
-    plt.fill_between(ell, c1, c1_l, color="b", alpha=0.5)
-    plt.fill_between(ell, c2, c2_l, color="b", alpha=0.8)
+    plt.fill_between(ell, c1, c1_l, color="royalblue", alpha=0.6)
+    plt.fill_between(ell, c2, c2_l, color="cornflowerblue", alpha=0.4)
 
     plt.savefig("../results/confidence_intervals.png", bbox_inches="tight")
     plt.show()
